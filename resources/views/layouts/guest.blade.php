b164f759af88a1d48a1aa1e3e75764afba8f9168
EC2 Default User <ec2-user@ip-172-31-51-106.ec2.internal>
2024-10-02 01:43:28 +0000

    Title&icon

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>CheerParK</title>
+        <title>{{ config('app.name', 'Laravel') }}CheerParK</title>
         
         <!-- JPG形式のファビコンを設定 -->
         <link rel="icon" href="{{ asset('storage/cheerpark.jpg') }}" type="image/jpeg">
